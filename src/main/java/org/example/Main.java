75124eb0d6017aa9e5397ef936020afc7ad22a75
0310199910031999 <jony.nim@gmail.com>
2023-08-28 21:14:36 -0600

    Creacion de sistema de libros

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,24 +4,22 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-    List<Libro> lista_Libros = new ArrayList<>();
-
-    Libro milibro1 = new Libro();
-    milibro1.setId(1);
-    lista_Libros.add(milibro1);
-    milibro1 = new Libro();
-    milibro1.setId(2);
-    lista_Libros.add(milibro1);
-    milibro1 = new Libro();
-    milibro1.setId(3);
-    lista_Libros.add(milibro1);
-    for (Libro libroActual: lista_Libros) {
-        System.out.println("Id: " + libroActual.getId());
+        List<Libro> lista_Libros = new ArrayList<>();
+        Libro milibro1 = new Libro();
+        milibro1.setId(1);
+        lista_Libros.add(milibro1);
+         for (int i = 1; i < 10; i++){
+             milibro1 = new Libro();
+             milibro1.setId(i+1);
+             milibro1.setNombre("Nombre"+ i+1);
+             lista_Libros.add(milibro1);
+         }
+        lista_Libros.add(milibro1);
+        for (Libro libroActual: lista_Libros) {
+            if(libroActual.getId() % 2 == 0){
+                System.out.println("Id: " + libroActual.getId() + "\nNombre: " + libroActual.getNombre());
+                System.out.println("--------------------------------------------------------------");
+            }
+        }
     }
-
-
-    }
-
-
-
 }
