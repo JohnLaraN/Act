0408f47e18d10e37f5c84262282262b0e539df67
0310199910031999 <jony.nim@gmail.com>
2023-08-28 20:36:22 -0600

    Borre lista

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,21 +6,25 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+    List<Libro> lista_Libros = new ArrayList<>();
+
+    Libro milibro1 = new Libro();
+    milibro1.setId(1);
+    lista_Libros.add(milibro1);
+    milibro1 = new Libro();
+    milibro1.setId(2);
+    lista_Libros.add(milibro1);
+    milibro1 = new Libro();
+    milibro1.setId(3);
+    lista_Libros.add(milibro1);
 
 
-        int decision = 0;
 
-        decision = Integer.parseInt(JOptionPane.showInputDialog("Que deseas realizar?: " +
-                 "\n 1) Ingresar un libro" +
-                "\n 2) Mostrar lista de libros"));
-        switch (decision){
-            case 1:
-                break;
-            case 2:
-                break;
-            default:
-                break;
+        for (Libro libroActual: lista_Libros) {
+            System.out.println("Id: " + libroActual.getId());
         }
+
+
     }
 
 
